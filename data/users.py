40985dd6d4e504f46f863acf7b8937fcc116f452
DaniilAnisimov <daniilanisimov2005@yandex.com>
2021-03-15 14:48:21 +0400

    Fourth version Added authorization

--- data/users.py
+++ data/users.py
@@ -4,9 +4,10 @@ from sqlalchemy import orm
 
 from .db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = "users"
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True)
@@ -25,4 +26,4 @@ class User(SqlAlchemyBase):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+        return check_password_hash(self.password, password)
--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
+
 from forms.user import RegisterForm
+from forms.login import LoginForm
+
+from main import main
+
 from data import db_session
 from data.users import User
+
 from re import *
-from main import main
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "123"  # in json
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
@@ -26,14 +41,33 @@ def index():
     return render_template("index.html", list_of_posts=list_of_posts_from_db, is_login='false')
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return "login"
+    user_from_db = {'nickname': 'Саня'}  # данные о залогиненном пользователе отправятся на представление
+    list_with_posts_from_db = {"ультрафиолетовое излучение": "λ < 380нм",
+                               "Инфракрасное излучение": "λ > 760нм",
+                               "Гамма излучение": "λ = 2 * 10^-10нм и прочее описание",
+                               "Рентгеновское излучение": 'Рентгеновское излучение - '
+                                                          'электромагнитные волны с '
+                                                          'длиной волны от 100 до 10^-3 нм'}  # Словарь постов из бд
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.address == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template("login.html", message="Неправильный логин или пароль",
+                               form=form, user=user_from_db, list_with_posts=list_with_posts_from_db, is_login='true')
+    return render_template("login.html", form=form, user=user_from_db, list_with_posts=list_with_posts_from_db, is_login='true')
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    return "logout"
+    logout_user()
+    return redirect("/")
 
 
 @app.route("/register", methods=['GET', 'POST'])
